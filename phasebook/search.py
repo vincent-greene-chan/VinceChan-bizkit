29903b668ce1bf605c85d6a1b6a30fc4458a3794
vincent-greene-chan <vincent.greene.chan@gmail.com>
2024-10-11 10:31:41 +0800

    search.py

--- phasebook/search.py
+++ phasebook/search.py
@@ -26,5 +26,28 @@ def search_users(args):
     """
 
     # Implement search here!
+    user_query = []
 
-    return USERS
+    id_filter = request.args.get("id")
+    name_filter = request.args.get("name")
+    age_filter = request.args.get("age")
+    occupation_filter = request.args.get("occupation")
+    
+
+    if id_filter:
+        user_query = [user for user in USERS if user['id'] == id_filter]
+
+    if name_filter:
+        user_query = user_query + [user for user in USERS if name_filter.lower() in user['name'].lower() and user not in user_query]
+        
+    if age_filter:
+        age_input = int(args['age'])
+        user_query = user_query + [user for user in USERS if age_input -1 <= user['age'] <= age_input +1 and user not in user_query]
+
+    if occupation_filter:
+        user_query = user_query + [user for user in USERS if occupation_filter.lower() in user['occupation'].lower() and user not in user_query]
+
+    if not id_filter and not name_filter and not age_filter and not occupation_filter:
+        user_query= USERS
+
+    return user_query
